88ce25d3d887de3a511a3f5af149776bb72ee41c
Alex Corn <mckay@doctormckay.com>
2019-02-27 17:11:48 -0500

    Added user-agent header

--- MyQClient.cs
+++ MyQClient.cs
@@ -51,6 +51,8 @@ namespace HSPI_LiftMasterMyQ
 
 		public MyQClient(MyQMake make = MyQMake.LiftMaster) {
 			httpClient = new HttpClient();
+			httpClient.DefaultRequestHeaders.Add("User-Agent",
+				"Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36");
 
 			if (make == MyQMake.LiftMaster || make == MyQMake.Chamberlain) {
 				httpClient.BaseAddress = new Uri(BASE_URL);
